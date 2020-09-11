081ba040219707a3b2027d7100fa475976c79b4d
Victor-Morvy <morvy0@gmail.com>
2020-09-11 17:45:51 -0300

    commit unico

---
+++ unidade09/_js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$('.carousel').carousel({
+		interval:2000, //muda o intervado de mudança para 2s
+		pause: "null", //nao vai parar de rolar se eu colocar o mouse em cima, de padrão ele para
+		wrap: true //quando chegar no final, vai parar de rolar
+
+	})
+
+});
